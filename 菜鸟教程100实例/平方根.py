1c0ccaa2efea0494fefe7eaaea03d4c392aca37e
MyPocketFullOfMoney <741788978@qq.com>
2020-07-26 12:42:02 +0800

    Create 平方根.py
    求解正数的平方根

---
+++ 菜鸟教程100实例/平方根.py
@@ -0,0 +1,15 @@
+#coding=utf-8 Python 3.7.4
+''' 
+ * Description  : 计算一个数字的平方根
+ * Author       : Lichongyou
+ * Date         : 2020-07-26 12:30:25
+ * LastEditTime : 2020-07-26 12:37:34
+ * LastEditors  : Lichongyou
+''' 
+
+#利用python的特殊符号：**
+# x ** y表示：x的y次方;那么求平方根， x ** 0.5
+
+x = float(input('请输入一个数字：'))
+
+print('该数的平方根为：',x ** 0.5)
