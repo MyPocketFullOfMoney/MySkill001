ab2adc313ec3c13b36c4dd0a99b8b4a507f2c118
MyPocketFullOfMoney <741788978@qq.com>
2020-07-28 19:18:27 +0800

    Create 7_随机数生成.py
    利用random.randint生成随机数

---
+++ 菜鸟教程100实例/7_随机数生成.py
@@ -0,0 +1,12 @@
+#coding=utf-8 Python 3.7.4
+''' 
+ * Description  : 利用random模块生成随机数
+ * Author       : Lichongyou
+ * Date         : 2020-07-28 10:51:26
+ * LastEditTime : 2020-07-28 10:58:55
+ * LastEditors  : Lichongyou
+''' 
+
+import random
+
+print('输出0-9之间的随机数：',random.randint(0,9))
