eab9dff8ec84255c380b1b6fef9ab2b49e329ec3
MyPocketFullOfMoney <741788978@qq.com>
2020-07-28 21:14:36 +0800

    Create 14_获取最大值函数.py

---
+++ 菜鸟教程100实例/14_获取最大值函数.py
@@ -0,0 +1,15 @@
+#coding=utf-8 Python 3.7.4
+''' 
+ * Description  : 利用Python自带的max函数获取最大值
+ * Author       : Lichongyou
+ * Date         : 2020-07-28 21:06:50
+ * LastEditTime : 2020-07-28 21:14:25
+ * LastEditors  : Lichongyou
+''' 
+
+list = [10,20,20,30,50,60,100]
+tuple = (10,20,20,30,50,60,80)
+
+print('{0}的最大值为：{1}'.format(list,max(list)))
+print('{0}的最大值为：{1}'.format(tuple,max(tuple)))
+
