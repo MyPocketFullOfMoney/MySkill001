57afa5492c8b5d01d0fa16d46eb40467167a55a8
MyPocketFullOfMoney <741788978@qq.com>
2020-10-20 09:25:32 +0800

    利用Python内置函数转换字符串大小写

---
+++ 菜鸟教程100实例/30_字符串大小写转换.py
@@ -0,0 +1,10 @@
+# 利用Python内置函数，转换字符大小写
+# 时间：2020/10/20
+
+str = 'www.runoob.com'
+print('测试字符串：',str)
+
+print('所有字符转换为大写：',str.upper())
+print('所有字符转换为小写字母：',str.lower())
+print('首字母转换为大写，其余小写：',str.capitalize())
+print('把每个单词的首字母大写，其余小写：',str.title())
