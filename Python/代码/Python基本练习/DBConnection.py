c7953900457e3caa47efd83e63945f3c88197c5e
MyPocketFullOfMoney <741788978@qq.com>
2021-06-08 00:03:10 +0800

    Create DBConnection.py
    创建MySQL连接，利用上下文管理器管理

---
+++ Python/代码/Python基本练习/DBConnection.py
@@ -0,0 +1,39 @@
+import pymysql
+
+
+class MysqlDBDriver(object):
+    '创建MySQL连接，利用上下文管理连接'
+
+    def __init__(self, m_host, m_user, m_pwd, m_db):
+        setting = {
+            'host': m_host,
+            'user': m_user,
+            'password': m_pwd,
+            'database': m_db
+        }
+        # 创建连接
+        self.db_con = pymysql.connect(**setting)
+
+        # 获取游标
+        self.db_cur = self.db_con.cursor()
+
+    # 返回游标
+    def __enter__(self):
+        return self.db_cur
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        # 关闭前先提交
+        self.db_con.commit()
+
+        # 关闭游标
+        self.db_cur.close()
+
+        # 关闭数据库连接
+        self.db_con.close()
+
+
+with MysqlDBDriver('localhost','root','123456','data_li') as li_cur:
+    li_cur.execute('select * from EMPLOYEE')
+    result_tuple = li_cur.fetchall()
+    print('结果：')
+    print(result_tuple)
