f96367342df9dc4cc857b740cc03af7b1966285a
MyPocketFullOfMoney <741788978@qq.com>
2021-07-26 00:02:50 +0800

    增加题目——替换空格

---
+++ Leetcode/ReplaceSpace.py
@@ -0,0 +1,13 @@
+class Solution:
+    def replaceSpace(self, s: str) -> str:
+
+        res = []
+
+        for i in s:
+
+            if i == ' ':
+                res.append('%20')
+            else:
+                res.append(i)
+        
+        return ''.join(res)
