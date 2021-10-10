c84dfebd30f32813c21fbe4bf7fe3fa632a4759f
MyPocketFullOfMoney <741788978@qq.com>
2021-10-10 23:17:44 +0800

    Update StringBaseOpr.cpp
    C++遍历字符串

--- C++/代码练习/src/StringBaseOpr.cpp
+++ C++/代码练习/src/StringBaseOpr.cpp
@@ -8,6 +8,7 @@ using namespace std;
 // 函数声明
 int ReadString();
 int GetlineString();
+int ForString();
 
 int main()
 {
@@ -16,6 +17,7 @@ int main()
     cout << s1 << s2 << endl; // 输出两个string对象
     ReadString();
     GetlineString();
+    ForString();
     return 0;
 }
 
@@ -23,9 +25,11 @@ int ReadString()
 {
     string word;
     cout << "cin 00 exit ReadString() function!!!" << endl;
-    while (cin >> word){
+    while (cin >> word)
+    {
         cout << word << endl;
-        if ("00" == word){
+        if ("00" == word)
+        {
             break;
         }
     }
@@ -38,6 +42,23 @@ int GetlineString()
     string line;
     // 每次读入一整行，直至到达文件尾。字符串对象line不存储换行符。
     while (getline(cin, line))
+    {
         cout << line << endl;
+        if ("00" == line)
+        {
+            break;
+        }
+    }
+    return 0;
+}
+
+int ForString()
+{
+    cout << "This is ForString() function." << endl;
+    string s("Hello World!!!");
+    // 抓换成大写形式
+    for (auto &c : s)   // 对于s中的每个字符（注意：c是引用）
+        c = toupper(c); // c是一个引用，因此赋值语句将改变s中字符的值
+    cout << s << endl;
     return 0;
 }
